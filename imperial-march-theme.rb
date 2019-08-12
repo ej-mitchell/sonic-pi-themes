ccb28997efc11aa69d1c4a7b0de172fbbf2fa9b7
EJ <eliza.j.mitchell@gmail.com>
2019-08-12 10:52:08 -0400

    Added functions to make this more reusable
    First theme has more subdivisions as its parts are the most reused
    portion of the song, in terms of familiarity. The second theme needs a
    bit of work to break down coherently.

--- imperial-march-theme.rb
+++ imperial-march-theme.rb
@@ -1,11 +1,6 @@
-# first theme
+# Partition definitions
 
-3.times do
-  play :G5
-  sleep 0.6
-end
-
-2.times do
+define :main_chord_inversion do
   play :Ef5
   sleep 0.45
   play :Bf5
@@ -14,78 +9,89 @@ end
   sleep 0.6
 end
 
-3.times do
-  play :D6
-  sleep 0.6
+define :first_theme_a do
+  3.times do
+    play :G5
+    sleep 0.6
+  end
+  
+  2.times do
+    main_chord_inversion
+  end
 end
 
-play :Ef6
-sleep 0.45
-play :Bf5
-sleep 0.15
-play :G5
-sleep 0.6
-
-1.times do
-  play :Ef5
+define :first_theme_b do
+  3.times do
+    play :D6
+    sleep 0.6
+  end
+  
+  play :Ef6
   sleep 0.45
   play :Bf5
   sleep 0.15
   play :G5
   sleep 0.6
+  
+  main_chord_inversion
 end
 
-# second theme
+define :first_theme do
+  first_theme_a
+  first_theme_b
+end
 
-play :G6
-sleep 0.6
-play :G5
-sleep 0.45
-play :G5
-sleep 0.15 
-play :G6
-sleep 0.6
-play :Gf6
-sleep 0.45
-play :F6
-sleep 0.15
-
-play :E6
-sleep 0.15
-play :Ds6
-sleep 0.15
-play :E6
-# NOTE: double sleeps are a reminder to add differentiation between duration and rest marks
-sleep 0.30
-sleep 0.30
-play :Gs5
-sleep 0.30
-play :Cs6
-sleep 0.6
-play :Bs5
-sleep 0.45
-play :B5
-sleep 0.15 
-
-play :Bf5
-sleep 0.15
-play :A5
-sleep 0.15
-play :Bf5
-sleep 0.30
-sleep 0.30
-play :Ef5
-sleep 0.30
-play :Gf5
-sleep 0.6
-
-# resolve on first theme
-
-2.times do
-  play :Ef5
+define :second_theme do
+  play :G6
+  sleep 0.6
+  play :G5
   sleep 0.45
+  play :G5
+  sleep 0.15 
+  play :G6
+  sleep 0.6
+  play :Gf6
+  sleep 0.45
+  play :F6
+  sleep 0.15
+
+  play :E6
+  sleep 0.15
+  play :Ds6
+  sleep 0.15
+  play :E6
+  # NOTE: double sleeps are a reminder to add differentiation between duration and rest marks
+  sleep 0.30
+  sleep 0.30
+  play :Gs5
+  sleep 0.30
+  play :Cs6
+  sleep 0.6
+  play :Bs5
+  sleep 0.45
+  play :B5
+  sleep 0.15 
+
   play :Bf5
   sleep 0.15
-  play :G5
+  play :A5
+  sleep 0.15
+  play :Bf5
+  sleep 0.30
+  sleep 0.30
+  play :Ef5
+  sleep 0.30
+  play :Gf5
   sleep 0.6
+
+  # resolve on first theme
+
+  2.times do
+    main_chord_inversion
+  end
 end
+
+## TO PLAY
+first_theme
+sleep 0.6
+second_theme
