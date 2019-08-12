5e5f7c6f5b78f0bf7ca83f8d4e22ab4157c09624
EJ <eliza.j.mitchell@gmail.com>
2019-08-12 09:59:48 -0400

    Readme with project intent and rough draft theme
    The Imperial March theme is a draft. TODO: Use features from Sonic Pi to
    make this much more DRY. Looking forward to this being a playground for
    other themes.

---
+++ README.md
@@ -0,0 +1,11 @@
+## What is this?
+
+A repository of familiar themes and original works that have been written out in Ruby files. These Ruby files are a good starting place for creating and improvising upon larger Sonic Pi projects. As a note, some of the familiar themes I've done by ear and others I was able to find open source music that gave me the notes/rhythm. If there are issues with how I've done some of the familiar themes, please open up an issue (or a PR with the fix) and I would be happy to review.
+
+## How do you use it?
+
+Say you want to do a remix of a famous _Star Wars_ theme. Say you didn't have the time to write it all out for Sonic Pi yourself. This is just a repo of time-savers and creative departures to make music reference easier. Copy and paste the themes you like into your project, and adjust them at you will.
+
+## Sonic Pi?
+
+Take a look at the GitHub repo [here](https://github.com/samaaron/sonic-pi) and the app site [here](https://sonic-pi.net/).
---
+++ imperial-march-theme.rb
@@ -0,0 +1,91 @@
+# first theme
+
+3.times do
+  play :G5
+  sleep 0.6
+end
+
+2.times do
+  play :Ef5
+  sleep 0.45
+  play :Bf5
+  sleep 0.15
+  play :G5
+  sleep 0.6
+end
+
+3.times do
+  play :D6
+  sleep 0.6
+end
+
+play :Ef6
+sleep 0.45
+play :Bf5
+sleep 0.15
+play :G5
+sleep 0.6
+
+1.times do
+  play :Ef5
+  sleep 0.45
+  play :Bf5
+  sleep 0.15
+  play :G5
+  sleep 0.6
+end
+
+# second theme
+
+play :G6
+sleep 0.6
+play :G5
+sleep 0.45
+play :G5
+sleep 0.15 
+play :G6
+sleep 0.6
+play :Gf6
+sleep 0.45
+play :F6
+sleep 0.15
+
+play :E6
+sleep 0.15
+play :Ds6
+sleep 0.15
+play :E6
+# NOTE: double sleeps are a reminder to add differentiation between duration and rest marks
+sleep 0.30
+sleep 0.30
+play :Gs5
+sleep 0.30
+play :Cs6
+sleep 0.6
+play :Bs5
+sleep 0.45
+play :B5
+sleep 0.15 
+
+play :Bf5
+sleep 0.15
+play :A5
+sleep 0.15
+play :Bf5
+sleep 0.30
+sleep 0.30
+play :Ef5
+sleep 0.30
+play :Gf5
+sleep 0.6
+
+# resolve on first theme
+
+2.times do
+  play :Ef5
+  sleep 0.45
+  play :Bf5
+  sleep 0.15
+  play :G5
+  sleep 0.6
+end
